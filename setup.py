790a7d8693d7599321ec9e9276b6ccd5241a60f5
NinhVDT <duyninh.vdt@gmail.com>
2024-09-20 10:55:06 +0700

    added the superdense coding and teleportation notebooks, both using qiskit one

--- setup.py
+++ setup.py
@@ -1,2 +1,5 @@
-from qiskit import IBMQ
-IBMQ.save_account("e57284b24d2033f21800735dd8544e995c549ec89e5d10f1f60ba9eedb895eacb4653df2be519cd194c1d12653df97aa1789ca8d74b1eeb7906c9116b277cabf", overwrite=True)
+from qiskit_ibm_runtime import QiskitRuntimeService
+# IBM Quantum channel; set to default 
+ 
+QiskitRuntimeService.save_account(channel="ibm_quantum", token="e57284b24d2033f21800735dd8544e995c549ec89e5d10f1f60ba9eedb895eacb4653df2be519cd194c1d12653df97aa1789ca8d74b1eeb7906c9116b277cabf", overwrite=True)
+print("Success!")
